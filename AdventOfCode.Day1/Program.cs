e1be74a4ab11874ffbc504030583ccfbf9d97a0a
Raka Schipperheijn <raka@rpkschipperheijn.com>
2024-12-01 22:02:36 +0100

    added part two of day 1 and refactored

--- AdventOfCode.Day1/Program.cs
+++ AdventOfCode.Day1/Program.cs
@@ -1,25 +1,37 @@
-﻿const string pathname = "input.txt";
-
-var (leftList, rightList) = ParseInput(pathname);
-
-var distance = leftList.Zip(rightList, (left, right) => Math.Abs(left - right)).Sum();
-Console.WriteLine($"Distance: {distance}");
+﻿const string fileName = "input.txt";
+var (left, right) = ParseInput(fileName);
+ComputeDistance(left, right);
+ComputeSimilarity(left, right);
 return;
 
-(List<int> left, List<int> right) ParseInput(string filename)
+static void ComputeDistance(List<int> leftList, List<int> rightList)
 {
-        var left = new List<int>();
-        var right = new List<int>();
+        var distance = leftList.Zip(rightList, (left, right) => Math.Abs(left - right)).Sum();
+        Console.WriteLine($"Distance: {distance}");
+}
+
+static void ComputeSimilarity(List<int> leftList, List<int> rightList)
+{
+        var frequencyDictionary = rightList.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        var similarity = leftList.Sum(num => num * frequencyDictionary.GetValueOrDefault(num,0));
+        Console.WriteLine($"Similarity: {similarity}");
+}
+
+static (List<int> leftList, List<int> rightList) ParseInput(string fileName)
+{
+        var leftList = new List<int>();
+        var rightList = new List<int>();
         
-        foreach(var line in File.ReadLines(filename))
+        foreach(var line in File.ReadLines(fileName))
         {
                 var numbers = line.Split([' '], StringSplitOptions.RemoveEmptyEntries);
-                left.Add(int.Parse(numbers[0]));
-                right.Add(int.Parse(numbers[1]));
+                leftList.Add(int.Parse(numbers[0]));
+                rightList.Add(int.Parse(numbers[1]));
         }
         
-        left.Sort();
-        right.Sort();
+        leftList.Sort();
+        rightList.Sort();
         
-        return (left, right);
+        return (leftList, rightList);
 }
+
